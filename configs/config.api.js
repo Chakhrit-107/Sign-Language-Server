1c5451b74908d5a402528b56bc1575737e862a70
Chakhrit-107 <Chakhrit.Phoowamukda@gmail.com>
2023-11-28 17:24:43 +0700

    Revert "Remove configs folder"
    This reverts commit b78cb192ffda95efbb7165ccf8813652b6bba9d4.

---
+++ configs/config.api.js
@@ -0,0 +1,6 @@
+module.exports = {
+  URL: "https://sign-language-server.onrender.com",
+  // PROTOCOL: "https",
+  // HOST: "sign-language-server.onrender.com",
+  PORT: "8800",
+};
---
+++ configs/config.database.js
@@ -0,0 +1,10 @@
+module.exports = {
+  user: "adminbest",
+  password: "adminB_041244",
+  server: "educationsignlanguage.database.windows.net",
+  database: "educationSignLanguage",
+  trustServerCertificate: true,
+  options: {
+    encrypt: true,
+  },
+};
