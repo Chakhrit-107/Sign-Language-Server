b78cb192ffda95efbb7165ccf8813652b6bba9d4
Chakhrit-107 <Chakhrit.Phoowamukda@gmail.com>
2023-11-28 17:10:48 +0700

    Remove configs folder

--- configs/authentication.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  secretKey: "12345",
-  hashRound: 10,
-};
--- configs/config.api.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  URL: "https://sign-language-server.onrender.com",
-  // PROTOCOL: "https",
-  // HOST: "sign-language-server.onrender.com",
-  PORT: "8800",
-};
